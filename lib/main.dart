17f7dd1c5e48d77e899c9b5f7daa9dd51883bfc9
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2024-09-15 03:55:53 +0900

    ディレクトリ構成を調整

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:adit/model/member/member.dart';
 import 'package:adit/model/member/member_name.dart';
 import 'package:adit/model/member/mind_lebel.dart';
 import 'package:adit/router/app_router.dart';
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -34,43 +33,6 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: AppRouter().config(),
-    );
-  }
-}
-
-@RoutePage()
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          MemberDetailHeaderView(),
-          FilledButton(
-              onPressed: () async {
-                AutoRouter.of(context).push(const MemberListRoute());
-              },
-              child: const Text('メンバー一覧画面へ')),
-          FilledButton(
-              onPressed: () async {
-                AutoRouter.of(context).push(const CreateMemberRoute());
-              },
-              child: const Text('メンバー作成画面へ'))
-        ],
-      ),
-    );
-  }
-}
-
-class MemberDetailHeaderView extends StatelessWidget {
-  const MemberDetailHeaderView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(
-      children: [Text('高山 龍馬'), Text('BN2023'), Text('自覚5'), Text('マインド5')],
     );
   }
 }
---
+++ lib/screen/home/home_view.dart
@@ -0,0 +1,40 @@
+import 'package:adit/router/app_router.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          MemberDetailHeaderView(),
+          FilledButton(
+              onPressed: () async {
+                AutoRouter.of(context).push(const MemberListRoute());
+              },
+              child: const Text('メンバー一覧画面へ')),
+          FilledButton(
+              onPressed: () async {
+                AutoRouter.of(context).push(const CreateMemberRoute());
+              },
+              child: const Text('メンバー作成画面へ'))
+        ],
+      ),
+    );
+  }
+}
+
+class MemberDetailHeaderView extends StatelessWidget {
+  const MemberDetailHeaderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [Text('高山 龍馬'), Text('BN2023'), Text('自覚5'), Text('マインド5')],
+    );
+  }
+}
